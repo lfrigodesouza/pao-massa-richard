798725eaf2d425e4a0d0dd50ce3a53c37a57da23
Lucas Frigo de Souza <lfrigodesouza@outlook.com>
2021-05-19 13:35:13 -0300

    Altera logo

--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 import React from 'react';
 import { number } from 'prop-types';
-import LogoImage from '../../assets/img/logo.jpeg';
+import LogoImage from '../../assets/img/logo.png';
 
 const LogoFrame = styled.img`
   width: ${(props) => props.width};
   height: ${(props) => props.height};
-  border-radius: 100%;
-  border: 2px solid var(--color-primary);
 `;
 
 export default function Logo({ width, height }) {
